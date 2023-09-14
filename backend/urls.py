9807f33f8a80a90297a535acc5899708fc49dab2
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-09-14 16:08:42 +0300

    static

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
-routes = getattr(settings, 'REACT_ROUTES', [])
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name='index.html')),
@@ -33,4 +32,4 @@ urlpatterns = [
 ] 
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, )
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
