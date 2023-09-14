758d33fc2f6dcf569c8090753c480f3c8f3e9024
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2023-09-14 16:14:09 +0300

    automatic mapping

--- backend/urls.py
+++ backend/urls.py
@@ -15,19 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
 
 urlpatterns = [
-    path("", TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path("auth/", include("UserAuthApp.urls")),
     path("payment/", include("PaymentsApp.urls")),
     path("courses_api/", include("CoursesApp.urls")),
     path("blog_api/", include("BlogApp.urls")),
+    re_path(r"^$", TemplateView.as_view(template_name='index.html')),
+    re_path(r"^(?:.*)/?$", TemplateView.as_view(template_name='index.html')),
     
 ] 
 
