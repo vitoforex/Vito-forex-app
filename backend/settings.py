a28082b3dfb0dffc7ab0e54a73dc90ff26136af1
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2024-01-26 16:36:08 +0300

    activated postgresql

--- backend/settings.py
+++ backend/settings.py
@@ -110,7 +110,7 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
@@ -129,7 +129,7 @@ DATABASES = {
         'PORT': '',
     }
 }
-"""
+
 
 
 
