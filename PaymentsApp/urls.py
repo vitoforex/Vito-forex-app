7ced1220444f86a5412f69353204184a2cb4eb6e
Joshua Bizinyika <joshuabiyinzika22@gmail.com>
2024-01-29 10:03:54 +0300

    removed payment app

--- PaymentsApp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('success', views.payment_successful, name='payment-successful'),
-    path('cancelled', views.payment_canceled, name='payment-cancelled'),
-    path('checkout', views.create_checkout_session, name='checkout'),
-    path('webhook', views.stripe_webhook, name='webhook'),
-]
